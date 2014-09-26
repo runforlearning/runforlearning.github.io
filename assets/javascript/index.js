fc7de753e2292f83dd194bd44527d357bb21be20
John Gill <john.p.gill@gmail.com>
2014-09-26 02:47:30 -0500

    Adds Race day photo carousel

---
+++ assets/javascript/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $("#race-day-photo-carousel").owlCarousel({
+
+    navigation: true,
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true,
+    lazyLoad: true,
+    autoHeight: true,
+    autoPlay: 4000,
+    stopOnHover: true
+
+  });
+
+});
